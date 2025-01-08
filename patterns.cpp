c8ad4d46608497512c9603c4139b27be24489f34
shreya918-git <shreyasharma918nds@gmail.com>
2025-01-08 18:50:22 +0530

    patterns

--- patterns.cpp
+++ patterns.cpp
@@ -23,11 +23,114 @@ using namespace std;
 //         cout<<endl;
 //     }
 // }
+// int main(){  //inverted star pattern
+//     int i;
+//     int n=5;
+//     for(i=n;i>=1;i--){
+//         for(int j=i;j>=1;j--){
+//             cout<<"*";
+//         }
+//         cout<<endl;
+//     }
+// // }
+// int main(){     //half pyramid pattern
+//     int i;
+//     int n=4;
+//     int j;
+//     for(i=1;i<=n;i++){
+//         for(j=1;j<=i;j++){
+//             cout<<i;
+//         }
+//         cout<<endl;
+//     }
+// }
+// int main(){
+//     int i;
+//     int n=4;
+//     int j;
+//     char ch='A';
+//     for(i=1;i<=n;i++){
+//         for(j=1;j<=i;j++){
+//           cout<<ch++; //post increment first outputs the current value and then update
+//         }
+//         cout<<endl;
+//     }
+// }
+// int main(){  //hollow rectangle pattern
+//     int i;
+//     int n=4;
+//     int j;
+//     for(i=1;i<=n;i++){
+//         cout<<"*";
+//         for(j=1;j<=n-1;j++){
+//             if(i==1||i==n){
+//                 cout<<"*";
+//             }
+//             else{
+//                 cout<<" ";
+//             }
+//         }
+//         cout<<"*";
+//         cout<<endl;
+//     }
+// }
+// int main(){  //flyodds triangle pattern
+//     int i;
+//     int n=4;
+//     int j;
+//     int k=1;
+//     for(i=1;i<=n;i++){
+//        for(j=1;j<=i;j++){
+//         cout<<k++;
+//        }
+//        cout<<endl;
+//     }
+// }
+// int main(){     //diamond pattern
+//     int i;
+//     int n=4;
+//     for(i=1;i<=n;i++){
+//         for(int j=1;j<=n-i;j++){  //for spaces we need to use another inner loop
+//             cout<<" ";
+//         }
+//         for(int j=1;j<=2*i-1;j++){
+//             cout<<"*";
+//         }
+//         cout<<endl;
+//     }
+//     for(i=n;i>=1;i--){
+//         for(int j=1;j<=n-i;j++){
+//             cout<<" ";
+//         }
+//         for(int j=1;j<=2*i-1;j++){
+//             cout<<"*";
+//         }
+//         cout<<endl;
+//     }
+// }
 int main(){
     int i;
-    int n=5;
+    int n=4;
+    for(i=1;i<=n;i++){
+        for(int j=1;j<=i;j++){
+            cout<<"*";
+        }
+        for(int j=1;j<=2*(n-i);j++){
+            cout<<" ";
+        }
+        for(int j=1;j<=i;j++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
     for(i=n;i>=1;i--){
-        for(int j=i;j>=1;j--){
+        for(int j=1;j<=i;j++){
+            cout<<"*";
+        }
+        for(int j=1;j<=2*(n-i);j++){
+            cout<<" ";
+        }
+        for(int j=1;j<=i;j++){
             cout<<"*";
         }
         cout<<endl;
